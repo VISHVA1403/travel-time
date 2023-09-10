1bc10003210c044050b921915313cd556dcc5fa6
VISHVA1403 <vishva28sep@gmail.com>
2023-09-10 16:24:28 +0530

    reseted

--- social/socialapi/forms.py
+++ social/socialapi/forms.py
@@ -1,8 +1,1 @@
 from django.forms import ModelForm
-from .models import UserProfile
-
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['profile_picture','cover_photo','bio','favorite_destinations','travel_style','wishlist']
--- social/socialapi/urls.py
+++ social/socialapi/urls.py
@@ -4,12 +4,4 @@ from . import views
 
 urlpatterns =[
     path('',views.home,name='home'),
-    path('profile/<int:pk>/', views.ProfilePage, name='profile'),
-    path('myprofile',views.myprofile,name='myprofile'),
-    path('users',views.users,name="users"),
-    path('profilecreate',views.EditProfile,name="create_profile"),
-    path('create-user',views.CreateUser,name = 'create-user'),
-    path('loginuser',views.userlogin,name='login'),
-    path('logoutuser',views.userlogout,name='logout'),
-    path("Detailapi",views.Userdetailapi,name='firstapi'),
 ]
